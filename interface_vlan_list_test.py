a72f9d6fedff5fe26344e18327df0535cd89dc20
Barkie <marat.kaz@gmail.com>
2016-05-31 15:58:11 +0300

    no global i

--- interface_vlan_list_test.py
+++ interface_vlan_list_test.py
@@ -98,17 +98,13 @@ for index, width in enumerate(widths):
 for index, header in enumerate(headers):
     ws.write(0, index, header)
 
-# create counter
-i = 1
 
-def search():
-    # w/o this thing counter do not work inside function
-    # BADPRACTICE/fix later!
-    global i
+def search(i):
     # find hostname
     hostname = find_device_hostname(some_str)
     if hostname:
         ws.write(i, 1, ''.join(hostname))
+
     # trying to find all interfaces with IP addresses
     vlanif = find_all_interfaces_with_ip(some_str)
     # for every interface run new searches
@@ -152,15 +148,21 @@ def search():
     print('info saved')
     # counter+1 to next excel line
     i += 1
-
     
+    return i
+
+
+row = 1
 for file in listFF:
+   # open file from folder file list
    f = open('C:/python/configs//{0}'.format(file), 'r+')
+   # read file content
    some_str = f.read()
-   print(i, file)
-   #write filename in first column
-   ws.write(i, 0, file)
-   search()
+   print(row, file)
+   # write filename in first column
+   ws.write(row, 0, file)
+   #row number = result of search function
+   row = search(row)
    print('ready')
    
 wb.save('C:/python/outputdir/interface_vlan_list_test.xls')
